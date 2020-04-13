963371d6631ef0a7bb7296b3e14449bfc0ba5f43
glenoxyde <61327386+glenoxyde@users.noreply.github.com>
2020-04-14 00:54:51 +0200

    Update constantes.h

--- constantes.h
+++ constantes.h
@@ -8,19 +8,19 @@
  *
  */
 
-//segment o� peut �voluer un joueur
-//du fait de la taille de l'�cran vis � vis de celle des rectangles des joueurs
+//segment ou peut evoluer un joueur
+//du fait de la taille de l'ecran vis a vis de celle des rectangles des joueurs
 #define UP_PLAYER_LIM 180
 #define DOWN_PLAYER_LIM 0
 
 
-//rectagle o� peut �voluer la ball
+//rectagle ou peut evoluer la ball
 #define UP_BALL_LIM 236
 #define DOWN_BALL_LIM 0
 #define RIGHT_BALL_LIM 312
 #define LEFT_BALL_LIM 0
 
-//adresses m�moires et longueurs des mots.
+//adresses memoire et longueurs des mots.
 #define P1WIN "P1 WINS"
 #define AD_P1WIN 1
 #define LEN_P1WIN 8
@@ -29,10 +29,10 @@
 #define AD_P2WIN 10
 #define LEN_P2WIN 8
 
-//adresse de sauvegrade de la derni�re victoire
+//adresse de sauvegrade de la derniere victoire
 #define SAVE_WIN 0
 
-//adresse et taille de la sauvegrade de l'�tat du jeu
+//adresse et taille de la sauvegrade de l'etat du jeu
 #define SAVE_P_LEN 12
 #define SAVE_P_ADD 100
 
