c938f4669dc9305c2c85b5d3f2d30da059829ddd
glenoxyde <61327386+glenoxyde@users.noreply.github.com>
2020-04-14 01:12:49 +0200

    Update timer.h

--- timer.h
+++ timer.h
@@ -12,8 +12,9 @@
 
 /**
 *@brief initialise les timers 1 et 2
-*	le timer 1 donne la fr�quence de base du son � produire
-*	le timer 2 donne la dur�e du son � produire.
-*
+*	le timer 1 donne la frequence de base du son a produire
+*	le timer 2 donne la duree du son a produire.
+* le timer 3 pourra etre utilise pour rythmer la musique 
+* 
 */
 void init_timer(void);
